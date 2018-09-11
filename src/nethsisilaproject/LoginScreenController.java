00668d3cc9d191df3ce7678cb83e2ade9989b3bf
gmalsha <malshag.gee@gmail.com>
2018-09-11 23:50:57 +0530

    Login done

--- src/nethsisilaproject/LoginScreenController.java
+++ src/nethsisilaproject/LoginScreenController.java
@@ -5,13 +5,26 @@
  */
 package nethsisilaproject;
 
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * FXML Controller class
@@ -21,9 +34,9 @@ import javafx.scene.control.Label;
 public class LoginScreenController implements Initializable {
 
     @FXML
-    private Button button;
+    private JFXTextField username;
     @FXML
-    private Label label;
+    private JFXTextField password;
 
     /**
      * Initializes the controller class.
@@ -34,7 +47,102 @@ public class LoginScreenController implements Initializable {
     }    
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void loginbutton(MouseEvent event) {
+        
+        boolean isExist = false;
+        String userPassword = "";
+        String userType = "";
+        
+        String query ="select * from logindetails where username='"+username.getText().toString().trim()+"'";
+         
+        Connection connection = DBConnection.getConnection();
+        
+        try {
+            PreparedStatement ps = (PreparedStatement)connection.prepareStatement(query);
+            
+            ResultSet rs=ps.executeQuery();
+            
+            while(rs.next()){
+                 isExist = true;
+                userPassword=rs.getString(3);
+                userType=rs.getString(5);
+            }
+            if (isExist) {
+                
+                if (password.getText().toString().trim().equals(userPassword)) {
+                    
+                    if(userType.equals("admin")){
+                        
+                        Stage AdminScreen = new Stage();
+                        
+                        Parent root = null;
+                        
+                           try {
+                            root=FXMLLoader.load(getClass().getResource("AdminScreen.fxml"));
+                        } catch (IOException ex) {
+                            Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                        Stage current = (Stage)username.getScene().getWindow();
+                         Scene scene=new Scene(root,1366,730);
+                        
+                        AdminScreen.setScene(scene);
+                        AdminScreen.initStyle(StageStyle.TRANSPARENT);
+                        
+                        current.hide();
+                        
+                        AdminScreen.show();
+                        
+                        
+                        AdminScreen.setScene(scene);
+                        AdminScreen.initStyle(StageStyle.TRANSPARENT);
+                        
+                        current.hide();
+                        
+                        AdminScreen.show();
+                        
+                        
+            }
+                    
+                
+                else  
+                    {
+                         Stage HomeScreen = new Stage();
+                        
+                        Parent root = null;
+                        
+                           try {
+                            root=FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
+                        } catch (IOException ex) {
+                            Logger.getLogger(LoginScreenController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                        Stage current = (Stage)username.getScene().getWindow();
+                         Scene scene=new Scene(root,1366,730);
+                        
+                       HomeScreen.setScene(scene);
+                        HomeScreen.initStyle(StageStyle.TRANSPARENT);
+                        
+                        current.hide();
+                        
+                     HomeScreen.show();
+                        
+                        
+                        HomeScreen.setScene(scene);
+                        HomeScreen.initStyle(StageStyle.TRANSPARENT);
+                        
+                        current.hide();
+                        
+                        HomeScreen.show();
+                        
+                } }
+            }
+            
+           
+        } catch (Exception e) {
+        }
+        
     }
     
 }
+  
